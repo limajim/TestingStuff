fd214cb9da83a7cba7aec0c9640b436288b984a2
Jim Van Dick <jvandick@paychex.com>
2016-08-22 13:48:47 -0400

    Added a Mock test - Continued

---
+++ General/Calculator/Interfaces/iKeyboard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Calculator.Interfaces
+{
+
+    public enum KeyboardType { Regular, Scientific };
+
+    public interface IKeyboard
+    {
+        void SetKeyboardType(KeyboardType keyboardType);
+        KeyboardType GetKeyboardType();
+    }
+}
